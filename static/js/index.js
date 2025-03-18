7a93c310e5df86860279651f55dbd0f7ab0d4d3f
jerredchen <jerredc99@gmail.com>
2025-03-18 22:38:04 +0000

    making autoplay faster

--- static/js/index.js
+++ static/js/index.js
@@ -60,7 +60,7 @@ $(document).ready(function() {
                 $('#animation-slider').prop('value', 0);
                 setAnimationImage(0);
             }
-        }, 500);
+        }, 300);
     }
 
     autoplaySlider(); // Start autoplay
